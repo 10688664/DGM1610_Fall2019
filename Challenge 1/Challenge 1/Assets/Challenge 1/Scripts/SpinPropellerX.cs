597564537ea12f99f830e1cd7bb2f9a86a746871
Pyrocube <desirae.stockwell@hotmail.com>
2019-09-18 17:14:25 -0600

    Added Challenge 1

---
+++ Challenge 1/Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float rotateSpeed = 100.0f;
+    public Vector3 propellerOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate = propeller.transform.position + propellerOffset;
+        //transform.Rotate(Vector3. * Time.deltaTime * rotateSpeed);
+    }
+}
