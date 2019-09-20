dcf3edee9d278dacb7e10f275d077b40929dbf08
Desirae Stockwell <desirae.stockwell@hotmaill.com>
2019-09-20 14:39:37 -0600

    Fixed Propeller Script

--- Challenge 1/Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Challenge 1/Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class SpinPropellerX : MonoBehaviour
 {
-    public float rotateSpeed = 100.0f;
-    public Vector3 propellerOffset;
+    private float rotateSpeed = 1000.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +15,7 @@ public class SpinPropellerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate = propeller.transform.position + propellerOffset;
-        //transform.Rotate(Vector3. * Time.deltaTime * rotateSpeed);
+        
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
     }
 }
