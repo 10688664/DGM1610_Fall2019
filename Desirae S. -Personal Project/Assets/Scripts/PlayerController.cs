828b0cf53efbc8513e561a8cf35bf15ea0bf676f
Desirae <desirae.stockwell@hotmail.com>
2019-11-01 17:37:22 -0600

    Player movement and boundaries

---
+++ Desirae S. -Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zBound = -18;
+    private float yBound = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        playerRb.AddForce(Vector3.up * speed * verticalInput);
+        playerRb.AddForce(Vector3.forward * speed * horizontalInput);
+
+        if (transform.position.z < zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+
+        if (transform.position.y > yBound)
+        {
+            transform.position = new Vector3(transform.position.x, yBound, transform.position.z);
+        }
+    }
+}
