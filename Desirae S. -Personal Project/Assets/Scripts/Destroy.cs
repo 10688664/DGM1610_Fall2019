1cf7f9c6f03095245174e55da604dff85ffca84d
Pyrocube <desirae.stockwell@hotmail.com>
2019-11-18 17:15:27 -0700

    Added projectile
    Added destroy script
    Added projectile movement
    Added Camera follow script
    Added Enemy Actions script

---
+++ Desirae S. -Personal Project/Assets/Scripts/Destroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private float zBounds = -18;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < zBounds)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //destroy projectile upon hit
+    private void OnTriggerEnter(Collider other) 
+    {
+        Destroy(gameObject);
+        Destroy(player);
+    }
+}
